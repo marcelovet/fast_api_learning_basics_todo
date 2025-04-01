071ad4e5461c7c328eec4227660598ecadc2876d
marcelo paiva <marcelo.paiva@crmvmg.gov.br>
2025-04-01 11:51:26 -0300

    basic user model

--- sqlite/models.py
+++ sqlite/models.py
@@ -1,7 +1,18 @@
-from sqlalchemy import TEXT, Boolean, Column, Integer, String
+from sqlalchemy import TEXT, Boolean, Column, Integer, String, ForeignKey
 
 from sqlite.database import Base
 
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    email = Column(String, unique=True, nullable=False)
+    username = Column(String(256), unique=True, nullable=False)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 class Todo(Base):
     __tablename__ = "todo"
@@ -11,3 +22,4 @@ class Todo(Base):
     description = Column(TEXT)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
