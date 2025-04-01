18b46c95dac6b925ab81d57bebf02a3cf029cf64
marcelo paiva <marcelo.paiva@crmvmg.gov.br>
2025-04-01 12:10:52 -0300

    a model for creating a user

--- request_models.py
+++ request_models.py
@@ -1,4 +1,23 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, EmailStr, Field, StringConstraints
+from typing_extensions import Annotated
+
+Username = Annotated[
+    str,
+    StringConstraints(
+        min_length=5,
+        max_length=15,
+        pattern=r"^[a-zA-Z0-9_]+$",  # Only letters, numbers, underscore
+    ),
+]
+
+
+class CreateUserRequest(BaseModel):
+    email: EmailStr
+    username: Username
+    first_name: str
+    last_name: str
+    password: str
+    role: str
 
 
 class TodoRequest(BaseModel):
