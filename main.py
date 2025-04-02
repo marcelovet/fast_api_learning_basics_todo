e094320230067f247a9725009174424c904cd388
marcelo paiva <marcelo_thelin@hotmail.com>
2025-04-01 21:09:46 -0300

    admin route

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
+from routers import admin, auth, todo
 from sqlite import models
 from sqlite.database import engine
-from routers import auth, todo
 
 app = FastAPI()
 
@@ -10,3 +10,4 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todo.router)
+app.include_router(admin.router)
---
+++ routers/admin.py
@@ -0,0 +1,41 @@
+from typing import Annotated
+
+from fastapi import APIRouter, HTTPException, Path
+from sqlalchemy import select
+from starlette import status
+
+from sqlite import models
+from sqlite.service import db_dependency, user_dependency
+
+router = APIRouter(
+    prefix="/admin",
+    tags=["admin"],
+)
+
+
+@router.get("/todo")
+async def read_all(user: user_dependency, db: db_dependency):
+    if user is None or user.get("user_role") != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication failed"
+        )
+    return db.scalars(  # type: ignore[attr-defined]
+        select(models.Todo)
+    ).all()
+
+
+@router.delete("/todo/delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(
+    user: user_dependency, db: db_dependency, id: Annotated[int, Path(gt=0)]
+):
+    if user is None or user.get("user_role") != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication failed"
+        )
+    todo = db.scalar(select(models.Todo).where(models.Todo.id == id))
+    if not todo:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found"
+        )
+    db.delete(todo)
+    db.commit()
