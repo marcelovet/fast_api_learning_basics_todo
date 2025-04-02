4528e17a4cc7a32f724ed3733de5a3a5a4e219a9
marcelo paiva <marcelo.paiva@crmvmg.gov.br>
2025-04-02 14:08:11 -0300

    mocking data in db

--- mock/mock_data.py
+++ mock/mock_data.py
@@ -1,19 +1,62 @@
-from faker import Faker
+from random import choice
 
+from faker import Faker
+from sqlalchemy import select
+
+from auth.security import pwd_context
 from database.config import DbSession
-from models.database_models import Todo
+from models.database_models import Todo, Users
 
 faker = Faker()
 
 
 def create_fake_data():
     with DbSession() as session:
-        for _ in range(10):
+        users = session.scalars(select(Users).where(Users.role != "admin")).all()
+        if not users:
+            return
+        for _ in range(100):
             todo = Todo(
                 title=faker.sentence(nb_words=10),
                 description=faker.text(),
                 priority=faker.random_int(min=1, max=5),
                 complete=faker.boolean(),
+                owner_id=choice(users).id,
             )
             session.add(todo)
+        session.commit()
+
+
+def create_fake_users():
+    emails = set([faker.email() for _ in range(100)])
+    usernames = set([faker.user_name() for _ in range(100)])
+    with DbSession() as session:
+        for _ in range(10):
+            user = Users(
+                email=emails.pop(),
+                username=usernames.pop(),
+                first_name=faker.first_name(),
+                last_name=faker.last_name(),
+                hashed_password=pwd_context.hash(faker.password()),
+                is_active=True,
+                role="user",
+                phone_number=faker.phone_number(),
+            )
+            session.add(user)
+        session.commit()
+
+
+def create_fake_admin():
+    with DbSession() as session:
+        admin = Users(
+            email=faker.email(),
+            username="admin",
+            first_name=faker.first_name(),
+            last_name=faker.last_name(),
+            hashed_password=pwd_context.hash("admin"),
+            is_active=True,
+            role="admin",
+            phone_number=faker.phone_number(),
+        )
+        session.add(admin)
         session.commit()
