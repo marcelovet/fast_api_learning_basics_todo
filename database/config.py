f2373efaa32695256748d3626d0c3fe8a7c17ff4
marcelo paiva <marcelo.paiva@crmvmg.gov.br>
2025-04-02 12:51:45 -0300

    postgres config

--- database/config.py
+++ database/config.py
@@ -1,10 +1,17 @@
+import urllib.parse
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
-)
+from setup import settings as st
+
+CONN_STRING = "postgresql+psycopg2://"
+CONN_STRING += f"{urllib.parse.quote_plus(st.POSTGRES_USER)}"  # type: ignore[attr]
+CONN_STRING += f":{urllib.parse.quote_plus(st.POSTGRES_PASSWORD)}@"  # type: ignore[attr]
+CONN_STRING += f"{st.POSTGRES_HOST}:{st.POSTGRES_PORT}/"  # type: ignore[attr]
+CONN_STRING += f"{st.POSTGRES_DB}"  # type: ignore[attr]
+
+engine = create_engine(CONN_STRING)
 DbSession = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
